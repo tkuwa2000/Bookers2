81deabd394dc5a937da642aff6027e35909d1575
takumi <tkuwa0605@gmail.com>
2022-10-01 15:35:12 +0000

    [Add] CRUDの追加

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  # 投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:text, :opinion)
+  end
+end
