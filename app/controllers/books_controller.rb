fb0c075cdc448cd6f5bb41199cd17b2ad37e401c
takumi <tkuwa0605@gmail.com>
2022-10-03 13:20:07 +0000

    [Add] Edit&Destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-  
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -17,8 +13,26 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_user = User.find(@book.user_id)
+    @user = current_user
   end
-  
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
   # 投稿データのストロングパラメータ
   private
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @book = Book.new
   end
 
   def index
@@ -11,5 +10,18 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
+  end
+  
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
