6c83291838f84cdc34ba0cc01471521e752cf261
takumi <tkuwa0605@gmail.com>
2022-10-03 16:55:07 +0000

    [Add] バリデーショの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,18 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully." 
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
     @user = current_user
   end
@@ -23,8 +30,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
+  
+  #titleとopinionが入力されているか確認
+  validates :title, presence: true
+  validates :opinion, presence: true
 end
